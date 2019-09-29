596c285d1a0451e8d5a4668445891590ce56b179
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-30 00:32:21 +0100

    remount all route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const userRoute = require('./users.route');
+const adminRoutes = require('./admin.route');
+const authRoute = require('./auth.route');
+const teamRoute = require('./team.route');
+const fixtureRoutes = require('./fixture.route');
+const searchRoutes = require('./search.route');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Everything is going to be alright!');
-});
+/* GET health-check - check service health */
+router.get('/health-check', (req, res) =>
+  res.send('Everything is going to be alright!'),
+);
+
+// mount user routes at /user
+router.use('/user', userRoute);
+
+// mount admin route
+router.use('/admin', adminRoutes);
+
+// mount admin route
+router.use('/auth', authRoute);
+
+// mount team route
+router.use('/team', teamRoute);
+
+// mount fixture route
+router.use('/fixture', fixtureRoutes);
+
+// mount search
+router.use('/search', searchRoutes);
 
 module.exports = router;
